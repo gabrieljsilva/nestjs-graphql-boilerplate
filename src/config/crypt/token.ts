b3b80f17227deb622b638ce511cfd6e380db05b7
Gabriel Silva <45879107+gabrieljsilva@users.noreply.github.com>
2022-06-29 18:00:43 -0300

    Update token.ts

--- src/config/crypt/token.ts
+++ src/config/crypt/token.ts
@@ -1,10 +1,10 @@
 import { randomInt } from 'crypto';
 
-export function generateToken(size: number): Promise<string> {
-  return new Promise((resolve) => {
-    const chars =
+const chars =
       'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
 
+export function generateToken(size: number): Promise<string> {
+  return new Promise((resolve) => {
     let token = '';
     do token += chars[randomInt(chars.length)];
     while (token.length < size);
